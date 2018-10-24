4e14c4a98300e2aacb2162fe0f96bcd5ac549adf
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-24 23:38:23 +0200

    Initial Commit

---
+++ iOSstopWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSstopWatch
+//
+//  Created by Bassam Mednini on 24.10.18.
+//  Copyright © 2018 Bassam Mednini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
