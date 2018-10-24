e0e9b00fdf4998ae36a9e02e2acf74d20229e9f6
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-24 23:56:26 +0200

    Buttons/Labels mit VC verknüpft, Funktionen erstellt

--- iOSstopWatch/ViewController.swift
+++ iOSstopWatch/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var timer: Timer?
+    var isStarted = false
+    var counter = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @objc func updateTimerLabel() {
+        
+    }
 
-
+    @IBAction func startStopButtonTouch(_ sender: UIButton) {
+        
+        if isStarted {
+            
+        } else {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimerLabel), userInfo: nil, repeats: true)
+            isStarted = true
+            startStopButton.setTitle("Stop", for: .normal)
+        }
+        
+    }
+    
+    
+    @IBAction func resetTouch(_ sender: UIButton) {
+        
+    }
+    
 }
 
